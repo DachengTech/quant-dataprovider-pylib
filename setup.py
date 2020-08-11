8c53a823beddb1dd7720a40b7850b74d7396af00
HarryLee <formaten@qq.com>
2020-08-11 14:36:40 +0800

    Added files for package setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='quant-dataprovider-pylib',
+    version='0.0.1-SNAPSHOT',
+    packages=[''],
+    url='',
+    license='',
+    author='DachengTech',
+    author_email='',
+    description=''
+)
